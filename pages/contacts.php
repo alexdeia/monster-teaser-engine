3cbed925a7f4ba38aafc6c670dc2aaa05aeff919
alexdeia <alexk.deia@gmail.com>
2017-02-16 13:58:24 +0300

    edit pages & fix contacts

--- pages/contacts.php
+++ pages/contacts.php
@@ -3,52 +3,47 @@
 =============================================================
 =============================================================
 MTE - Monster Teaser Engine
+eTeaser @ 2017
 Author: unknown
 Refactoring: Alexey Klykov
-Contacts: http://chronodev.ru
+Contacts: http://chronodev.ru | http://aklykov.ru
 E-mail: alexk.deia@gmail.com
 =============================================================
 =============================================================
 */
 
 $str = '
-<div class="page_content">
 	<h1 class="title">Контакты</h1>
-    <div id="cont">
-    	Вы всегда можете связаться с нами по следующим координатам:
+    	Вы всегда можете связаться с нами по следующим контактам:
     	<br/><br/>
-    	<div style="padding-left: 20px;">
+    	<p>
     		<b>email:</b> <a href="mailto:'.$sys['admin_email'].'">'.$sys['admin_email'].'</a>
      		<br/>
      		<b>ICQ:</b> <img src="http://status.icq.com/online.gif?icq='.$sys['admin_icq'].'&img=5"> '.$sys['admin_icq'].'
      		<br/>
-			<b>Webmoney ID:</b> <a href="wmk:msgto?to='.$sys['wmid'].'&amp;subject=contact">'.$sys['wmid'].'</a> BL:<img src="http://stats.wmtransfer.com/Levels/pWMIDLevel.aspx?wmid='.$sys['wmid'].'&amp;w=35&amp;h=18&amp;bg=0XDBE2E9" alt="" border="0" height="18" width="35">
+			<b>Webmoney ID:</b> <a href="wmk:msgto?to='.$sys['wmid'].'&amp;subject=contact">'.$sys['wmid'].'</a> BL:<img src="https://bl.wmtransfer.com/img/bl/'.$sys['wmid'].'?w=45&h=18&bg=0XDBE2E9" alt="" border="0" height="18" width="35">
      		<br/>
-     	</div>
-     		Вы также можете отправить нам сообщение, используя следующую форуму:
+     	</p>
+     		<p>Вы также можете отправить нам сообщение, используя следующую форуму:</p>
      		<div style="padding-left: 20px;">
 	     		<form action="/send-message.html" method="post">
-				<table cellpadding="0" width="520" cellspacing="5">
+				<table class="table">
 					<tr>
-						<td><label for="name">���� ���</label></td><td><input type="text" name="name" id="name" autocomplete="off" style="width: 200px;"></td>
+						<td><label for="name">Имя</label></td><td><input class="form-control" type="text" name="name" id="name" autocomplete="on" style="width: 200px;"></td>
 					</tr>
-					<tr><td colspan="2" height="1" bgcolor="#e4e4e4"></td></tr>
 					<tr>
-						<td><label for="email">��� email</label></td><td><input type="text" name="email" id="email" autocomplete="off" style="width: 200px;"></td>
+						<td><label for="email">Email</label></td><td><input class="form-control" type="text" name="email" id="email" autocomplete="on" style="width: 200px;"></td>
 					</tr>
-					<tr><td colspan="2" height="1" bgcolor="#e4e4e4"></td></tr>
 					<tr>
-						<td valign="top"><label for="email">���� ���������</label></td>
-						<td><textarea name="message" id="message" style="width: 400px; height: 200px;"></textarea></td>
+						<td valign="top"><label for="email">Ваше сообщение</label></td>
+						<td><textarea class="form-control" name="message" id="message" style="width: 400px; height: 200px;"></textarea></td>
 					</tr>
 					<tr>
 						<td></td>
-						<td><input type="submit" class="button" value="Отправить сообщение"></td>
+						<td><input type="submit" class="btn btn-primary" value="Отправить сообщение"></td>
 					</tr>
 				</table>
 	     		</form>
      		</div>
-     	</div>
-	</div>
 ';
 ?>
